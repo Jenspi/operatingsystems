f8de90e632abc736d24d96ad51d91fa890f3f2c7
Jennifer Spicer <xJenniferSpicer@gmail.com>
2023-12-01 18:39:54 -0600

    [Unfinished] Now sends output directly to file, rather than terminal

--- HW4/Deadlock.java
+++ HW4/Deadlock.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -19,6 +21,8 @@ public class Deadlock{
             File file = new File(args[0]);
             Scanner scanner = new Scanner(file);
             RAG graph = new RAG();
+            String outputname = args[0].replace("input", "output");
+            FileWriter outputFile = new FileWriter(outputname);
         
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
@@ -27,37 +31,34 @@ public class Deadlock{
                 String action = columns[1].trim();
                 String resource = columns[2].trim();
                 
-                Node processNode = graph.newNode(process);
-                Node resourceNode = graph.newNode(resource);
+                Node processNode = graph.newNode(process, true);
+                Node resourceNode = graph.newNode(resource, false);
 
                 if(action.toLowerCase().equals("w")){
-                    //wants-- two outcomes: process successfully allocated, or process must wait on another process taking the resource
-                    System.out.print("Process "+process+" wants resource "+resource+" – ");
-                    graph.requestEdge(processNode, resourceNode);
-                    // if(resourceNode.nextNode.isEmpty()){
-                        
-                    //     System.out.println("Resource "+resource+" is allocated to process "+process+".");
-                    // }
-                    // else{
-                    //     System.out.println("Process "+process+" must wait.");
-                    // }
-
+                    // wants-- two outcomes: process successfully allocated, or process must wait on another process taking the resource
+                    outputFile.write("Process "+process+" wants resource "+resource+" – ");
+                    
+                    outputFile.write(graph.requestEdge(processNode, resourceNode));
                 }
                 else if(action.toLowerCase().equals("r")){
-                    //releases-- one outcome: resource released
-                    System.out.print("Process "+process+" releases resource "+resource+" – ");
+                    // releases-- one outcome: resource released
+                    outputFile.write("Process "+process+" releases resource "+resource+" – ");
                     graph.removeEdge(processNode, resourceNode);
-                    System.out.println("Resource "+resource+" is now free.");
+                    resourceNode.setBusy(false);
+                    outputFile.write("Resource "+resource+" is now free.\n");
                 }
             }
-            graph.deadlockPresence();
-            // if(graph.deadlockPresence()){
-            //     System.out.println("DEADLOCK DETECTED: ");//Processes 2, 3, 4, and Resources 2, 3, 6, are found in a cycle.");
-            // }
+            //print of there was a deadlock or not:
+            outputFile.write( graph.deadlockPresence() );
             
+            outputFile.close();
+            System.out.println("Output created: "+ outputname);
             scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + args[0]);
+        }
+        catch (IOException e) {
+            System.out.println("I/O Exception!");
         }
         catch (java.lang.ArrayIndexOutOfBoundsException a) {
             System.out.println("No file specified!");
@@ -67,17 +68,42 @@ public class Deadlock{
 
 class RAG {
     //here lies code to implement your Resource Allocation Graph data structure class ...
-    //https://www.geeksforgeeks.org/resource-allocation-graph-rag-in-operating-system/
+    // https://www.geeksforgeeks.org/resource-allocation-graph-rag-in-operating-system/
     //Your program will track resource requests, allocations, and releases
     public ArrayList<Node> edge = new ArrayList<Node>();
 
-    public Node newNode(String name){
-        Node node = new Node(name);
-        //add to edges so deadlock helper functions can access it
+    public Node newNode(String name, boolean isResource){
+        Node node = new Node(name, isResource);
         edge.add(node);
-        //add node to Node class so we can check later if that nextNode list is empty
-        //node.addNode(node);
         return node;
+    }
+
+    //edges:
+    public String requestEdge(Node process, Node resource){
+        if(resource.nodeList.isEmpty()){
+        //if(!resource.isBusy()){
+
+            //if resource is available
+            addEdge(resource, process);
+            // DEBUGGING:
+            // System.out.println("EMPTY resource list: ");
+            // resource.printList();
+            ////////////////////////////////////////////////
+            resource.setBusy(true);
+            return "Resource "+resource.name+" is allocated to process "+process.name+".\n";
+            
+        }
+        else if(!resource.nodeList.isEmpty()){
+        //else if(resource.isBusy()){
+
+            //if resource pre-occupied
+            // DEBUGGING:
+            // System.out.println("FULL resource list: ");
+            // resource.printList();
+            ////////////////////////////////////////////////
+            return "Process "+process.name+" must wait.\n";
+        }
+        return "";
     }
     public void addEdge(Node current, Node next){
         //need directed egdes!!
@@ -87,55 +113,40 @@ class RAG {
     }
     public void removeEdge(Node current, Node next){
         current.removeNode(next);
+        edge.remove(next);
     }
-    public void requestEdge(Node process, Node resource){
-        if(resource.nextNode.isEmpty()){
-            //if resource is available
-            resource.addNode(process);
-            System.out.println("Resource "+resource.name+" is allocated to process "+process.name+".");
-        }
-        else if(!resource.nextNode.isEmpty()){
-            //if resource pre-occupied   
-            //process.addNode(resource);
-            System.out.println("Process "+process.name+" must wait.");
-        }
-    }
-    public boolean deadlockPresence(){
+    
+    //deadlocks:
+    public String deadlockPresence(){
         //note: not necessary to check for deadlock after a resource release
         ArrayList<Node> visited = new ArrayList<Node>();
         ArrayList<Node> visited2 = new ArrayList<Node>();
 
         for(Node node : edge){
             if(!visited.contains(node)){
-                //means
                 if(deadlockCycle(node, visited, visited2)){
-                    //true
-
-                    //go through all processes
-
-                    System.out.println("DEADLOCK");
-                    return true;
+                    //true; deadlock detected when entering this block
+                    return "DEADLOCK DETECTED";
+                    //return true;
                 }
             }
         }
         //false
-        System.out.println("EXECUTION COMPLETED: No deadlock encountered.");
-        return false;
+        return "EXECUTION COMPLETED: No deadlock encountered.";
+        //return false;
     }
     public boolean deadlockCycle(Node node, ArrayList<Node> visited, ArrayList<Node> visited2){
-        //helper function for deadlockPresence
+        //recursive helper function for deadlockPresence
         visited.add(node);
         visited2.add(node);
-            for(Node next : node.nextNode){
+            for(Node next : node.nodeList){
             if(!visited.contains(next)){
-                //means
                 if(deadlockCycle(next, visited, visited2)){
-                    //true
                     return true;
                 }
             }
             else if(visited.contains(next)){
-                //true
+                //CYCLE
                 return true;
             }
         }
@@ -143,24 +154,44 @@ class RAG {
         visited2.remove(node);
         return false;
     }
-
 }//end RAG class
 
 class Node {
     //here lies the code for a node object, ...
     public String name;
-    public ArrayList<Node> nextNode = new ArrayList<Node>();
+    public ArrayList<Node> nodeList = new ArrayList<Node>();
+    boolean isResource, isBusy;
 
-    public Node(String name){
+    public Node(String name, boolean isResource){
         this.name = name;
+        this.isResource = isResource;
+        if(isResource){
+            this.isBusy = false;
+        }
+        else{this.isBusy=true;}
     }
 
     public void addNode(Node node){
-        nextNode.add(node);
+        nodeList.add(node);
     }
     public void removeNode(Node node){
-        nextNode.remove(node);
+        nodeList.remove(node);
     }
-    
+    public void printList(){
+        for(Node node : nodeList){
+            System.out.print(node.name+" ");
+        }
+        System.out.println("\n");
+    }
+    public ArrayList<Node> getList(Node node){
+        return node.nodeList;
+    }
+    public boolean isBusy(){
+        //ONLY FOR RESOURCES
+        return this.isBusy;
+    }
+    public void setBusy(boolean bool){
+        this.isBusy = bool;
+    }
 }//end Node class
     
